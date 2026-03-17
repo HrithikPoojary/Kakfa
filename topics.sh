05d217c6e874f6ce127cc794b4ae90e249875b12
HrithikPoojary <hrithikpoojary29@gmail.com>
2026-03-17 08:58:57 +0000

    Topic creation

---
+++ topics.sh
@@ -0,0 +1,14 @@
+To Create Topics
+
+>bin/kafka-topics.sh \
+>--create \
+>--topic <topic_name> \
+>--bootstrap-server localhost:9092 \
+>--partitions <number_of_partitions (0<) \
+>--replication-factor <number of replications>
+
+To list the Topics
+
+>bin/kafka-topics.sh \
+>--list \
+>--bootstrap-server localhost:9092
