dc6c7e05e74d95fa99213deac633c1c4f7132fee
HrithikPoojary <hrithikpoojary29@gmail.com>
2026-03-17 08:43:13 +0000

    Set up commands

---
+++ commands.sh
@@ -0,0 +1,12 @@
+To Start the Zookeper
+>bin/zookeeper-server-start.sh config/zookeeper.properties
+
+To Start the Kafka
+>bin/kafka-server-start.sh /config/server.properties
+
+
+To check Running Servers
+>jps
+
+To Kill the Server
+> kill<jps ID> 
